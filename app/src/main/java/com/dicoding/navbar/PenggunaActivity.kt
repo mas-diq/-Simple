40fddeb27d81afdca8be8dd81193ca45b270a0a1
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-03-06 00:46:43 +0700

    Adding some "item" to move between activity

---
+++ app/src/main/java/com/dicoding/navbar/PenggunaActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.navbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PenggunaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengguna)
+    }
+}
