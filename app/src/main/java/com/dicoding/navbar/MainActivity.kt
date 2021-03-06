781e32ef14e451a3a1ce94d8ce28c0a440a5916e
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-03-07 01:51:49 +0700

    Huge progress!

--- app/src/main/java/com/dicoding/navbar/MainActivity.kt
+++ app/src/main/java/com/dicoding/navbar/MainActivity.kt
@@ -3,23 +3,33 @@ package com.dicoding.navbar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttomNavigationView = findViewById<BottomNavigationView>(R.id.buttom_navigation)
-        val navController = findNavController(R.id.fragmentBeranda)
-        buttomNavigationView.setupWithNavController(navController)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(
+                setOf(
+                    R.id.berandaFragment,
+                    R.id.ibuFragment,
+                    R.id.anakFragment,
+                    R.id.anakFragment,
+                    R.id.artikelFragment,
+                    R.id.penggunaFragment
+                )
+            )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
-    override fun onClick(v: View?) {
 
-    }
 }
